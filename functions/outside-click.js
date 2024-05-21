368a592b27faca2c161696a9f547925bfe558b73
Gabriel Henrique <ghbranco6@gmail.com>
2024-05-21 20:23:01 -0300

    feat dropdown-menu

---
+++ functions/outside-click.js
@@ -0,0 +1,22 @@
+export default function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const dataOutside = "data-outside";
+
+  if (!element.hasAttribute(dataOutside)) {
+    events.forEach((userEvent) => {
+      html.addEventListener(userEvent, handleOutsideClick);
+    });
+    element.setAttribute(dataOutside, "");
+  }
+
+  function handleOutsideClick(e) {
+    if (!element.contains(e.target)) {
+      element.removeAttribute(dataOutside);
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+      html.removeEventListener("click", handleOutsideClick);
+      callback();
+    }
+  }
+}
--- modules/dropdown-menu/dropdown-menu.js
+++ modules/dropdown-menu/dropdown-menu.js
@@ -1,3 +1,19 @@
-export default function dropdownMenu() {
+import outsideClick from "../../functions/outside-click.js";
 
+export default function dropdownMenu() {
+  const dropdownMenu = document.querySelector("[data-dropdown]");
+
+  const userEvents = ["touchstart", "click"];
+
+  userEvents.forEach((userEvent) => {
+    dropdownMenu.addEventListener(userEvent, handleClick);
+  });
+
+  function handleClick(e) {
+    e.preventDefault();
+    this.classList.add("active");
+    outsideClick(this, userEvents, () => {
+      this.classList.remove("active");
+    });
+  }
 }

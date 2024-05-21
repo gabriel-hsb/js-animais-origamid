6806ec66adb4ad0515339190c98f375f5ca29883
Gabriel <ghbranco6@gmail.com>
2024-05-21 14:54:30 -0300

    :card_file_box: organize folders and js module
    This commit improves the changes made in the commit b905575

---
+++ script.js
@@ -0,0 +1,11 @@
+import scrollToSection from "./modules/scroll-to-section/scroll-to-section.js";
+scrollToSection();
+
+import faqAccordions from "./modules/accordion-list/accordion-list.js";
+faqAccordions();
+
+import animateSectionEntrance from "./modules/section-entrance/animate-section-entrance.js";
+animateSectionEntrance();
+
+import animalsDescription from "./modules/animals-description/animals-description.js";
+animalsDescription();
